21a99cab6a7c0e0867bdec9e2c4b3ff644ab8e27
Zhuang Li <johnnn.li@mail.utoronto.ca>
2017-09-27 15:02:10 -0400

    - Finalized Main function

--- implementation.c
+++ implementation.c
@@ -214,8 +214,6 @@ void       tmUpdateVertex (tmMat4i* io_pMat){
         gVertex = ebot_left;
     else
         printf("Error: Update Vertex Fail");
-        
-    
     
 }
 /////// Tiling /////////////
@@ -968,6 +966,7 @@ void implementation_driver(struct kv *sensor_values, int sensor_values_count, un
         processed_frames += 1;
         if (processed_frames % 25 == 0) {
             tmTransformTiledMemory(gGlobalTiledMemory, gGlobalTransform);
+            tmUpdateVertex(gGlobalTransform);
             tmTiledMemoryToFrame(frame_buffer,width*height,gGlobalTiledMemory);
             verifyFrame(frame_buffer, width, height, grading_mode);
         }
