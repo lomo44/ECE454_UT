4ac2868f854825389b682b65f5fe62c600688286
Gujiang Lin <gujiang.lin@mail.utoronto.ca>
2017-11-25 18:17:19 -0500

    implement hash table

--- lab5/ggHashTable.c
+++ lab5/ggHashTable.c
@@ -1,14 +1,110 @@
 #include "ggHashTable.h"
 #include <stdlib.h>
-ggHashTable*    ggHashTable_Create(int m_iSize, ggeHashingAlgorithm in_eAlgorithm){
-    return NULL;
+
+/*
+ *Function: ggHashTable_Create
+ * ----------------------------
+ *  Initialize a hash function
+ *
+ *  in_eAlgorithm: a enum define the type of hashfunction
+ *
+ *  returns: the pointer to a hashtable
+ */
+ggHashTable*    ggHashTable_Create(ggeHashingAlgorithm in_eAlgorithm){
+    ggHashTable* hash_output = (ggHashTable*) malloc (sizeof(ggHashTable));
+    hash_output->m_eAlgorithm = ggeHashingAlgorithm_Default;
+    return hash_output;
 }
-void            ggHashTable_Free(ggHashTable* in_pHashTable){
+/*
+ *Function: ggHashTable_Free
+ * ----------------------------
+ *  Free the hash table 
+ *
+ *  in_pHashTable: pointer to the hashtable
+ */
+void ggHashTable_Free(ggHashTable* in_pHashTable){
+    free (in_pHashTable);
     return;
 }
-void*           ggHashTable_Search(ggHashTable* in_pHashTable, char* m_pKey){
+/*
+ *Function: ggHashTable_Search
+ * ----------------------------
+ *  Find a node store inside hashtable 
+ *
+ *  in_pHashTable: pointer to the hashtable
+ *  m_pNodeTL: the top-left child ndes
+ *  m_pNodeTR: the top-right child ndes
+ *  m_pNodeBL: the bottom-left child ndes
+ *  m_pNodeBR: the bottom-right child ndes
+ *  
+ *  returns: the pointer to the node
+ */
+ggQuadTreeNode* ggHashTable_Search( ggHashTable* in_pHashTable, 
+                                    ggQuadTreeNode* m_pNodeTL, 
+                                    ggQuadTreeNode* m_pNodeTR, 
+                                    ggQuadTreeNode* m_pNodeBL, 
+                                    ggQuadTreeNode* m_pNodeBR) {
+    //generate hashtag by cobime digit 2-17 of the child node adress;
+    ggHashKey m_pKey = ggKey((ggHashKey)m_pNodeTL,(ggHashKey)m_pNodeTR,(ggHashKey)m_pNodeBL,(ggHashKey)m_pNodeBR);
+    int index = ggHash(m_pKey);
+    ggHashTableEntry* node_output = in_pHashTable->m_pEntries[index];
+    //search the hashed entry in hash table 
+    while (node_output!= NULL){
+        if (node_output->m_pkey == m_pKey){
+            if (ggHashTable_CmpNode (node_output->m_pPayload,m_pNodeTL, m_pNodeTR, m_pNodeBL, m_pNodeBR)){
+                return node_output->m_pPayload;
+            }         
+        }
+        node_output = node_output->m_pNextEntry;
+    }
     return NULL;
 }
-void            ggHashTable_Insert(ggHashTable* in_pHashTable, char* m_pKey, void* m_pPayLoad){
+/*
+ *Function: ggHashTable_Insert
+ * ----------------------------
+ *  Find a node store inside hashtable 
+ *
+ *  in_pHashTable: pointer to the hashtable
+ *  m_pPayLoad: pointer to the data ndes
+ *  
+ */
+void ggHashTable_Insert(ggHashTable* in_pHashTable, ggQuadTreeNode* m_pPayLoad){
+    ggHashKey m_pkey = ggKey((ggHashKey)m_pPayLoad->m_pChildNodes[eQuadTreePosition_TL],
+                             (ggHashKey)m_pPayLoad->m_pChildNodes[eQuadTreePosition_TR],
+                             (ggHashKey)m_pPayLoad->m_pChildNodes[eQuadTreePosition_BL],
+                             (ggHashKey)m_pPayLoad->m_pChildNodes[eQuadTreePosition_BR]);
+    int index = ggHash(m_pkey);
+    ggHashTableEntry* node_temp = (ggHashTableEntry*) malloc (sizeof(ggHashTableEntry));
+    node_temp->m_pkey = m_pkey;
+    node_temp->m_pPayload = m_pPayLoad;
+    node_temp->m_pNextEntry = in_pHashTable->m_pEntries[index];
+    in_pHashTable->m_pEntries[index] = node_temp;
     return;
+}
+
+/*
+ *Helping Function: ggHashTable_CmpNode
+ * ----------------------------
+ *  check if the parent node contains four child node 
+ *
+ *  m_pNodeTL: the top-left child ndes
+ *  m_pNodeTR: the top-right child ndes
+ *  m_pNodeBL: the bottom-left child ndes
+ *  m_pNodeBR: the bottom-right child ndes
+ *  
+ *  Return: return 1 if parent node contain four child nodes
+ */   
+ int ggHashTable_CmpNode (  ggQuadTreeNode* m_pNodeP,                                    
+                            ggQuadTreeNode* m_pNodeTL,
+                            ggQuadTreeNode* m_pNodeTR,
+                            ggQuadTreeNode* m_pNodeBL,
+                            ggQuadTreeNode* m_pNodeBR){
+    if ((m_pNodeP->m_pChildNodes[eQuadTreePosition_TL] == m_pNodeTL) &&
+        (m_pNodeP->m_pChildNodes[eQuadTreePosition_TR] == m_pNodeTR) &&
+        (m_pNodeP->m_pChildNodes[eQuadTreePosition_BL] == m_pNodeBL) &&
+        (m_pNodeP->m_pChildNodes[eQuadTreePosition_BR] == m_pNodeBR)) {
+        
+            return 1;
+    }     
+    return 0;
 }
--- lab5/ggHashTable.h
+++ lab5/ggHashTable.h
@@ -1,6 +1,7 @@
 #ifndef _ggHashTable_h
 #define _ggHashTable_h
 
+#include "ggQuadTree.h"
 /** Function definition for hash tables
  * Currently target implementation is hashing with chaining
  * Future improvement:
@@ -8,6 +9,12 @@
  * 2. Double hashing
  * 3. Multi-threading/ threadsafe hashing
  **/
+#define HASHTABLE_SIZE 4096
+#define ggHash(__key) (((__key)/37)%HASHTABLE_SIZE) 
+#define ggShift(__x,__shift) (((__x>>2)&0xFFFF)<<__shift)
+#define ggKey(__TL,__TR,__BL,__BR) ((ggShift(__TL,48))|(ggShift(__TR,32))|(ggShift(__BL,16))|(ggShift(__BR,0))) 
+
+typedef unsigned long long ggHashKey;
 
 typedef enum __ggeHashingAlgorithm{
     ggeHashingAlgorithm_Default,
@@ -15,24 +22,77 @@ typedef enum __ggeHashingAlgorithm{
 } ggeHashingAlgorithm;
 
 typedef struct __ggHashTableEntry{
-    char* m_pkey;
-    void* m_pPayload;
+    ggHashKey m_pkey;
+    ggQuadTreeNode* m_pPayload;
     struct __ggHashTableEntry* m_pNextEntry;
 } ggHashTableEntry;
 
 typedef struct __ggHashTable {
-    int m_iSize;
-    ggHashTableEntry* m_pEntries;
+    ggHashTableEntry* m_pEntries[HASHTABLE_SIZE];
     ggeHashingAlgorithm m_eAlgorithm;
 } ggHashTable;
-
-//TODO: Implement
-ggHashTable*    ggHashTable_Create(int m_iSize, ggeHashingAlgorithm in_eAlgorithm);
-//TODO: Implement
+/*
+ *Function: ggHashTable_Create
+ * ----------------------------
+ *  Initialize a hash function
+ *
+ *  in_eAlgorithm: a enum define the type of hashfunction
+ *
+ *  returns: the pointer to a hashtable
+ */
+ggHashTable*    ggHashTable_Create(ggeHashingAlgorithm in_eAlgorithm);
+/*
+ *Function: ggHashTable_Search
+ * ----------------------------
+ *  Find a node store inside hashtable 
+ *
+ *  in_pHashTable: pointer to the hashtable
+ *  m_pNodeTL: the top-left child ndes
+ *  m_pNodeTR: the top-right child ndes
+ *  m_pNodeBL: the bottom-left child ndes
+ *  m_pNodeBR: the bottom-right child ndes
+ *  
+ *  returns: the pointer to the node
+ */
+ggQuadTreeNode* ggHashTable_Search( ggHashTable* in_pHashTable,
+                                    ggQuadTreeNode* m_pNodeTL,
+                                    ggQuadTreeNode* m_pNodeTR,
+                                    ggQuadTreeNode* m_pNodeBL,
+                                    ggQuadTreeNode* m_pNodeBR);
+/*
+ *Function: ggHashTable_Insert
+ * ----------------------------
+ *  Find a node store inside hashtable 
+ *
+ *  in_pHashTable: pointer to the hashtable
+ *  m_pPayLoad: pointer to the data ndes
+ *  
+ */                                  
+void            ggHashTable_Insert(ggHashTable* in_pHashTable, ggQuadTreeNode* m_pPayLoad);
+/*
+ *Function: ggHashTable_Free
+ * ----------------------------
+ *  Free the hash table 
+ *
+ *  in_pHashTable: pointer to the hashtable
+ */
 void            ggHashTable_Free(ggHashTable* in_pHashTable);
-//TODO: Implement
-void*           ggHashTable_Search(ggHashTable* in_pHashTable, char* m_pKey);
-//TODO: Implement
-void            ggHashTable_Insert(ggHashTable* in_pHashTable, char* m_pKey, void* m_pPayLoad);
+/*
+ *Helping Function: ggHashTable_CmpNode
+ * ----------------------------
+ *  check if the parent node contains four child node 
+ *
+ *  m_pNodeTL: the top-left child ndes
+ *  m_pNodeTR: the top-right child ndes
+ *  m_pNodeBL: the bottom-left child ndes
+ *  m_pNodeBR: the bottom-right child ndes
+ *  
+ *  Return: return 1 if parent node contain four child nodes
+ */   
+ int            ggHashTable_CmpNode(    ggQuadTreeNode* m_pNodeP,                                    
+                                        ggQuadTreeNode* m_pNodeTL,
+                                        ggQuadTreeNode* m_pNodeTR,
+                                        ggQuadTreeNode* m_pNodeBL,
+                                        ggQuadTreeNode* m_pNodeBR);
 
 #endif
